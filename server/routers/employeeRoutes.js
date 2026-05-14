d8ccb1c51f98440a5041fbf287a8c28883fb03fd
manjusha-0103 <manjushakatkhede96@gmail.com>
2026-05-14 18:37:27 +0530

    API-> Appointment Approval

---
+++ server/routers/employeeRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {protect} from "../middlewares/authmiddleware.js";
+import { authorize } from "../middlewares/authoriseRoleMiddleware.js";
+import validate from "../middlewares/validate.js";
+
+import { chekIsApprove } from "../contollers/employeeController.js";
+
+const router = express.Router();
+
+
+router.get("/is-approve/:appointment_id", chekIsApprove)
+export default router;
+
