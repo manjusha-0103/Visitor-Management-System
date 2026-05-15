b7870cf2ab1e282a10bacb00b83c5ddeb315a767
manjusha-0103 <manjushakatkhede96@gmail.com>
2026-05-15 17:03:05 +0530

    API-> Dashboard(supperadmin, reception), socket->appointment dashboard(super_admin, reception)

--- server/services/dashboard.service.js
+++ server/services/dashboard.service.js
@@ -2,3 +2,96 @@ import bcrypt from "bcryptjs"
 import sql from "../db/database.js"
 import ApiError from "../utils/ApiError.js"
 import { sendEmail } from "../utils/mailer.js"
+
+const pastAppointmentsService = async ({
+            page,
+            limit
+        }) => {
+    const offset = (page - 1) * limit;
+
+    const [countResult] = await sql`
+        SELECT COUNT(*) AS total
+        FROM "Appointments" a
+        WHERE a.date_time::date < CURRENT_DATE
+    `;
+
+    const total = Number(countResult.total);
+
+    const amps = await sql`
+        SELECT
+            -- Appointment
+            a.id AS appointment_id,
+            a.created_at AS appointment_created_at,
+            a.check_in,
+            a.check_out,
+            a.date_time,
+            a.is_preschedule,
+            a.is_approve,
+
+            -- Employee
+            e.id AS employee_id,
+            e.position AS employee_position,
+            e.department,
+
+            -- Employee User
+            eu.first_name AS employee_first_name,
+            eu.last_name AS employee_last_name,
+            eu.email AS employee_email,
+            eu.phone AS employee_phone,
+
+            -- Visitor
+            v.id AS visitor_id,
+            v.position AS visitor_position,
+            v.company,
+            v.is_laptop,
+            v.laptop_make,
+            v.laptop_model,
+            v.laptop_serial_no,
+            v.is_vehicle,
+            v.vehicle_no,
+            v.pass_id,
+
+            -- Visitor User
+            vu.first_name AS visitor_first_name,
+            vu.last_name AS visitor_last_name,
+            vu.email AS visitor_email,
+            vu.phone AS visitor_phone
+
+        FROM "Appointments" a
+
+        JOIN "Employee" e
+            ON e.id = a.employee_id
+
+        JOIN "Visitors" v
+            ON v.id = a.visitor_id
+
+        JOIN "Users" eu
+            ON eu.id = e.user_id
+
+        JOIN "Users" vu
+            ON vu.id = v.user_id
+
+        WHERE a.date_time::date < CURRENT_DATE
+
+        ORDER BY a.date_time DESC
+
+        LIMIT ${limit}
+        OFFSET ${offset}
+    `;
+
+    return  {
+        data: amps,
+
+        pagination: {
+            total,
+            page,
+            limit,
+            totalPages: Math.ceil(total / limit),
+            hasNextPage: page < Math.ceil(total / limit),
+            hasPrevPage: page > 1
+        }
+    }
+}
+export{
+    pastAppointmentsService
+}
---
+++ server/services/receptionist.service.js
@@ -0,0 +1,3 @@
+import bcrypt from "bcryptjs"
+import sql from "../db/database.js"
+import ApiError from "../utils/ApiError.js"
