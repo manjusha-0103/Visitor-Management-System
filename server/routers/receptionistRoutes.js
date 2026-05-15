134846c203f8ace32515f2da9ff1de8ca8b6a5a4
manjusha-0103 <manjushakatkhede96@gmail.com>
2026-05-15 17:03:05 +0530

    API-> Dashboard(supperadmin, reception), socket->appointment dashboard(super_admin, reception)

--- server/routers/receptionistRoutes.js
+++ server/routers/receptionistRoutes.js
@@ -2,10 +2,9 @@ import express from "express";
 import {protect} from "../middlewares/authmiddleware.js";
 import { authorize } from "../middlewares/authoriseRoleMiddleware.js";
 import validate from "../middlewares/validate.js";
-import { pastAppointments } from "../contollers/dashboardController.js";
+
 
 const router = express.Router();
-
-router.get("/past",protect, authorize('super_admin', "receptionist"), pastAppointments)
+// router.get("/past",protect, authorize('super_admin', "receptionist"), pastAppointments)
 
 export default router;
