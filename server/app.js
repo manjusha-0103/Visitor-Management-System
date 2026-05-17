26250cf23b5cbdf77c79b8d1b409eb5d96cc59ea
manjusha-0103 <manjushakatkhede96@gmail.com>
2026-05-17 14:48:40 +0530

    API-> Update User ANd employee

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(cors({
     origin: process.env.CLIENT_DEV_URL,
     credentials: true
 }))
-
+app.use(express.json());
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
