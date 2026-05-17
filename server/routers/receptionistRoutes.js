93194082460162a895356460126c33ab34e0e0cb
manjusha-0103 <manjushakatkhede96@gmail.com>
2026-05-17 21:42:57 +0530

    API-> Reception(appointment Is aprove)

--- server/routers/receptionistRoutes.js
+++ server/routers/receptionistRoutes.js
@@ -3,12 +3,13 @@ import {protect} from "../middlewares/authmiddleware.js";
 import { authorize } from "../middlewares/authoriseRoleMiddleware.js";
 import validate from "../middlewares/validate.js";
 import { setPassId,
-    checkOut
+    checkOut,
+    checkIsApprove
  } from "../contollers/receptionistController.js";
 
 
 const router = express.Router();
 router.put("/set-pass/:appointment_id",protect, authorize( "receptionist"), setPassId)
 router.put("/check-out/:appointment_id", protect, authorize( "receptionist"), checkOut)
-
+router.put("/is-approve/:appointment_id", protect, authorize( "receptionist"), checkIsApprove)
 export default router;
