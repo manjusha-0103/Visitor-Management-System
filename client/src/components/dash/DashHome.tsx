db71e9dbde37fbdbd401813baa47b45748076877
r1414a <rupeshchincholkar14@gmail.com>
2026-05-18 13:13:39 +0530

    background color change for auth and dash

--- client/src/components/dash/DashHome.tsx
+++ client/src/components/dash/DashHome.tsx
@@ -9,8 +9,15 @@ export default function DashHome() {
   return (
     <div className="min-h-screen  p-6"
     style={{
-          background: 'linear-gradient(90deg, rgb(112, 26, 64) 0%, rgb(84, 11, 40) 100%)'
-    }}>
+  background: `
+    radial-gradient(
+      circle at 50% 0%,
+      var(--color-maroon) 0%,
+      var(--color-maroon-dark) 100%
+    )
+  `
+}}
+    >
       <div className="text-center mb-10">
         <p className="text-white/70 text-lg lg:text-xl mt-2 leading-6 tracking-tight">
           {
